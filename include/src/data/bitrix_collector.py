3eee43f364ec06a451267ccaa30b421759e1ac0c
Nicolas Nagel <nicolas.nagel@savecompany.com.br>
2026-02-04 10:48:52 -0300

    feat: adicionando 2 novos endpoints

--- include/src/data/bitrix_collector.py
+++ include/src/data/bitrix_collector.py
@@ -33,7 +33,7 @@ class BitrixCollector:
         self.SMART_PROCESS_ID = os.getenv('SMART_PROCESS_ID')
 
         self.URL = f'https://{self.SERVER_ADDRESS}/{self.RELATIVE_PATH}?token={self.SECRET_KEY}'
-        self.tables: List[str] = ['crm_deal', 'crm_deal_stage_history', 'user']
+        self.tables: List[str] = ['crm_deal', 'crm_deal_stage_history', 'user', 'crm_company', 'crm_stages']
         self.data: Dict[str, Any] = {}
 
     def start(self) -> None:
